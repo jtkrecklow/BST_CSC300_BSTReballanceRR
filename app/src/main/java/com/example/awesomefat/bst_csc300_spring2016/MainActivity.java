82ea45d9f0e3c31bad5326bd7b3186c4630a258a
jtkrecklow <gaul678@gmail.com>
2016-04-06 19:36:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/awesomefat/bst_csc300_spring2016/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.awesomefat.bst_csc300_spring2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BST tree = new BST();
+        String vals = "gfaaa";
+        for(int i = 0; i < vals.length(); i++)
+        {
+            tree.add(vals.charAt(i));
+        }
+
+
+        tree.visitPreOrder();
+
+        if(tree.isOutOfBalance())
+        {
+            System.out.println("Out of balance: " + tree.isOutOfBalance());
+            tree.howAreWeOutOfBalance('a');
+            tree.rebalance();
+            System.out.println("fixed it");
+        }
+
+        tree.visitPreOrder();
+
+
+
+        //tree.visitPreOrder();
+        //tree.visitPostOrder();
+        //tree.visitInOrder();
+        //tree.visitLevelOrder();
+    }
+}
